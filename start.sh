1406dd657673b15174714a910854e441e5b678dc
agomesd <alex.gduguay@gmail.com>
2024-03-27 14:36:07 +0000

    add debug to shell script

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 set -e
 
 echo "run db migration"
+# Debugging
+ls -l /app/app.env  # Check if the file exists and its permissions
+cat /app/app.env    # Print the content of app.env for debugging
+
 source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
